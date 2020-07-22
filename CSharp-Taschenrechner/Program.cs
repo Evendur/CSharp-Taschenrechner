6cd31c967b84943ad05d65bcd73e0ec8554e6e09
Evendur <Evendur@live.de>
2020-07-22 19:40:45 +0200

    Initiale Dateien hinzugefügt

---
+++ CSharp-Taschenrechner/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CSharp_Taschenrechner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
